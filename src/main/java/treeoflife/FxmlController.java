74f2684fadcd796c1da2c771c6e3aeecaff7efed
cookie_mac <xuzihansasc@163.com>
2024-04-12 15:00:52 +0200

    Add position and zoom functions

--- src/main/java/treeoflife/FxmlController.java
+++ src/main/java/treeoflife/FxmlController.java
@@ -2,9 +2,11 @@ package treeoflife;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Circle;
+import treeoflife.modele.Position;
 
 public class FxmlController {
     @FXML
@@ -39,5 +41,34 @@ public class FxmlController {
         stackPane.getChildren().add(newCircle);
         stackPane.getChildren().add(label);
         anchorPane.getChildren().add(stackPane);
+    }
+
+    public void zoomIn(){
+        anchorPane.setScaleX(anchorPane.getScaleX() * 1.1);
+        anchorPane.setScaleY(anchorPane.getScaleY() * 1.1);
+    }
+
+    public void zoomOut(){
+        anchorPane.setScaleX(anchorPane.getScaleX() / 1.1);
+        anchorPane.setScaleY(anchorPane.getScaleY() / 1.1);
+    }
+
+    boolean isPressed = false;
+    Position startPostion;
+    Position currentPosition;
+
+    public void pressed(MouseEvent e){
+        isPressed = true;
+        startPostion = new Position(e.getX(), e.getY());
+    }
+
+    public void released(MouseEvent e){
+        isPressed = false;
+        System.out.println("Released");
+    }
+
+    public void dragged(MouseEvent e){
+        currentPosition = new Position(currentPosition.getX(), currentPosition.getY());
+        System.out.println("Dragged");
     }
 }
